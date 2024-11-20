ce85efbcf8eb43f241640e1b4c9dd4cbf0c4af29
suwdle <suwdle1917@gmail.com>
2024-11-20 09:27:03 +0900

    three

--- Main.java
+++ Main.java
@@ -3,4 +3,5 @@ public class Main {
         // 자신의 학번과 이름을 입력하라 
         System.out.println("201514099 홍길동"); 
         System.out.println(Hello.sum(10)); 
-        System.out.println(Hello.sum(100)); } }
+        System.out.println(Hello.sum(100)); 
+        System.out.println(Hello.sum(1000)); } }
